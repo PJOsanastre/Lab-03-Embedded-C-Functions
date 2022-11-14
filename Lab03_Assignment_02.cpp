80e163dcd75beae1552517df5bd7ac17d1f675b8
PJOsanastre <117377417+PJOsanastre@users.noreply.github.com>
2022-11-15 00:33:21 +0800

    Lab03 Assignment 02
    2. Write a program to compute the area and perimeter of a rectangle with a width of three inches and a height of five inches. What changes must be made to the program so that it works for a rectangle with a width of 6.8 inches and a length of 2.3 inches?

---
+++ Lab03_Assignment_02.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+int findArea (float width, float height){
+    float area = width * height; 
+    return area;
+};
+
+int main(){
+
+printf("%d",findArea(6.8,2.3));//calling the area function
+
+
+}
